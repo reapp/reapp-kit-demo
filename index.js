0fb69980f66b193ec9064a69f5a5b866d2f60785
natew <natewienert@gmail.com>
2015-04-06 11:28:24 -0700

    betterer

--- index.js
+++ index.js
@@ -1,11 +1,4 @@
-import { React, Reapp, Immutable, List, Button, router, route, theme, store, actions } from 'reapp-kit';
-import iOSTheme from 'reapp-kit/themes/ios';
-
-theme({
-  constants: [iOSTheme.constants.base, iOSTheme.constants.components],
-  styles: [iOSTheme.styles],
-  animations: [iOSTheme.animations],
-});
+import { React, Reapp, List, Button, Cursor, router, store, actions } from 'reapp-kit';
 
 store({
   post: { 1: { title: 'Hello World', content: 'Lorem Ipsum' } },
@@ -13,31 +6,28 @@ store({
 });
 
 actions('addPost', (title, content) => {
+  const id = Math.random();
   store().withMutations(store => {
-    const id = Math.random();
-    store.setIn(['post', id], Immutable.fromJS({ title: 'Another', content: 'Lorem' }));
-    store.set('posts', store.get('posts').push(id));
+    store.setIn(['post', id], { title: 'Another', content: 'Lorem' });
+    store.get('posts').push(id);
   })
 });
 
-class Article extends React.Component {
+const Article = Cursor(['posts', 'post'], ArticleComponent);
+class ArticleComponent extends Reapp.Component {
   render() {
     return (
       <List>
-        {this.store().get('posts').map(id =>
-          <ArticleItem
-            post={this.store().getIn(['post', id])}
-          />
+        {this.props.posts.map(id =>
+          <ArticleItem post={this.props.post.get(id)} />
         )}
-        <Button fullscreen onTap={this.actions.addPost}>
-          Add Post
-        </Button>
+        <Button onTap={actions.addPost}>Add Post</Button>
       </List>
     );
   }
 }
 
-class ArticleItem extends React.Component {
+class ArticleItem {
   render() {
     return (
       <List.Item title={this.props.post.get('title')}>
@@ -47,6 +37,4 @@ class ArticleItem extends React.Component {
   }
 }
 
-router(
-  route('articles', '/', { handler: Reapp(Article) })
-);
+router({ path: '/', handler: Reapp(Article) });
